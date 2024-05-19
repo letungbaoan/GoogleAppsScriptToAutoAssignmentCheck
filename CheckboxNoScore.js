7771f36652ecec333c9348f23601548254d771e8
Lê Tùng Bảo Ân <baoan7604@gmail.com>
2024-05-19 09:25:51 +0700

    Upload scripts and document for using

---
+++ CheckboxNoScore.js
@@ -0,0 +1,38 @@
+function updateCheckboxes() {
+  const sheetAId = '1UwoyD3mC4b9ImxyFZmZo8heBR0-8aIRJRXKrhxF1_xA';
+  const tenSheetA = 'Sheet1';
+  const cotMSSVSheetA = 0;
+  const dongDuLieuDauTienSheetA = 2;
+  const cotCuoiCungCanDocSheetA = 'B';
+  const cotChuaCheckBox = 'B';
+
+  const sheetBId = '1A0jly0Z5afz9SCxAmlAEx4paPbvZKT21B50mu3HJfIY';
+  const tenSheetB = 'Sheet1';
+  const cotMSSVSheetB = 0;
+  const dongDuLieuDauTienSheetB = 2;
+  const cotCuoiCungCanDocSheetB = 'B';
+
+  const ssA = SpreadsheetApp.openById(sheetAId);
+  const sheetA = ssA.getSheetByName(tenSheetA);
+
+  const ssB = SpreadsheetApp.openById(sheetBId);
+  const sheetB = ssB.getSheetByName(tenSheetB);
+
+  const dataA = sheetA.getRange(`A${dongDuLieuDauTienSheetA}:${cotCuoiCungCanDocSheetA}` + sheetA.getLastRow()).getValues();
+  
+  const dataB = sheetB.getRange(`A${dongDuLieuDauTienSheetB}:${cotCuoiCungCanDocSheetB}` + sheetB.getLastRow()).getValues();
+
+  const studentsDoneAssignment = new Map();
+
+  dataB.forEach(row => {
+    const studentId = Number(row[cotMSSVSheetB]);
+    studentsDoneAssignment.set(studentId, true);
+  });
+
+  for (let i = 0; i < dataA.length; i++) {
+    const studentId = Number(dataA[i][cotMSSVSheetA]);
+    if (studentsDoneAssignment.has(Number(studentId))) {
+      sheetA.getRange(cotChuaCheckBox + (i + dongDuLieuDauTienSheetA)).setValue(true);
+    }
+  }
+}
---
+++ CheckboxWithScore.js
@@ -0,0 +1,43 @@
+function updateCheckboxes() {
+  const diemCanDeDat = 0.8;
+  const sheetAId = '1UwoyD3mC4b9ImxyFZmZo8heBR0-8aIRJRXKrhxF1_xA';
+  const tenSheetA = 'Sheet1';
+  const cotMSSVSheetA = 1;
+  const dongDuLieuDauTienSheetA = 9;
+  const cotCuoiCungCanDocSheetA = 'D';
+  const cotChuaCheckBox = 'D';
+
+  const sheetBId = '1A0jly0Z5afz9SCxAmlAEx4paPbvZKT21B50mu3HJfIY';
+  const tenSheetB = 'Sheet1';
+  const cotMSSVSheetB = 2;
+  const cotDiemSheetB = 5;
+  const dongDuLieuDauTienSheetB = 2;
+  const cotCuoiCungCanDocSheetB = 'F';
+
+  const ssA = SpreadsheetApp.openById(sheetAId);
+  const sheetA = ssA.getSheetByName(tenSheetA);
+
+  const ssB = SpreadsheetApp.openById(sheetBId);
+  const sheetB = ssB.getSheetByName(tenSheetB);
+
+  const dataA = sheetA.getRange(`A${dongDuLieuDauTienSheetA}:${cotCuoiCungCanDocSheetA}` + sheetA.getLastRow()).getValues();
+  
+  const dataB = sheetB.getRange(`A${dongDuLieuDauTienSheetB}:${cotCuoiCungCanDocSheetB}` + sheetB.getLastRow()).getValues();
+
+  const studentPassingScores = new Map();
+
+  dataB.forEach(row => {
+    const studentId = Number(row[cotMSSVSheetB]);
+    const score = Number(row[cotDiemSheetB]);
+    if (score >= diemCanDeDat) {
+      studentPassingScores.set(studentId, true);
+    }
+  });
+
+  for (let i = 0; i < dataA.length; i++) {
+    const studentId = Number(dataA[i][cotMSSVSheetA]);
+    if (studentPassingScores.has(Number(studentId))) {
+      sheetA.getRange(cotChuaCheckBox + (i + dongDuLieuDauTienSheetA)).setValue(true);
+    }
+  }
+}
